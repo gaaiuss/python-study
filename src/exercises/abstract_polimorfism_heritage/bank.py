68f02cfa81881bf168c7b2f88d647d3bcd0496dd
gaaiuss <caio.gui.castro@gmail.com>
2026-08-06 13:06:50 -0300

    add: auth logic

--- src/exercises/abstract_polimorfism_heritage/bank.py
+++ src/exercises/abstract_polimorfism_heritage/bank.py
@@ -1,27 +1,79 @@
 from typing import TYPE_CHECKING
 
+from exercises.abstract_polimorfism_heritage import bank
+from exercises.abstract_polimorfism_heritage.accounts import (
+    CheckingAccount,
+    SavingsAccount,
+)
+from exercises.abstract_polimorfism_heritage.person import Client
+
 if TYPE_CHECKING:
     from exercises.abstract_polimorfism_heritage.accounts import Account
-    from exercises.abstract_polimorfism_heritage.person import Client
 
 
 class Bank:
-    def __init__(self, agency: int) -> None:
-        self.agency = agency
-        self.accounts: list[Account] = []
-        self.clients: list[Client] = []
+    def __init__(
+        self,
+        agencies: list[int] | None = None,
+        accounts: list[Account] | None = None,
+        clients: list[Client] | None = None,
+    ) -> None:
+        self.agencies = agencies or []
+        self.accounts = accounts or []
+        self.clients = clients or []
 
-    def add_client(self, *args: Client) -> None:
-        for client in args:
-            self.clients.append(client)
+    def _agency_check(self, agency: int) -> bool:
+        if agency in self.agencies:
+            return True
 
-    def add_account(self, *args: Account) -> None:
-        for account in args:
-            self.accounts.append(account)
+        print(f"{agency} does not exist in this Bank")
+        return False
 
-    def auth(self, client: Client, account: Account) -> bool:
+    def _client_check(self, client: Client) -> bool:
+        if client in self.clients:
+            return True
+
+        print(f"{client} does not exist in this Bank")
+        return False
+
+    def _account_check(self, account: Account) -> bool:
+        if account in self.accounts:
+            return True
+
+        print(f"{account} does not exist in this Bank")
+        return False
+
+    def _client_account_check(self, client: Client, account: Account) -> bool:
+        if client.account == account:
+            return True
+
+        print(f"{account} does not belong to the {client}")
+        return False
+
+    def auth(self, agency: int, client: Client, account: Account) -> bool:
         return (
-            client in self.clients
-            and account in self.accounts
-            and self.agency == account.agency
+            self._agency_check(agency)
+            and self._client_check(client)
+            and self._account_check(account)
+            and self._client_account_check(client, account)
         )
+
+    def __repr__(self) -> str:
+        cls_name = type(self).__name__
+        attrs = f"{self.agencies!r}, {self.clients!r}, {self.accounts!r}"
+        return f"{cls_name}({attrs})"
+
+
+if __name__ == "__main__":
+    account = SavingsAccount(123, 123)
+    check_acc = CheckingAccount(321, 321)
+
+    gaius = Client("Gaius", 23, account)
+    john = Client("John", 23, check_acc)
+
+    bank = Bank()
+    bank.agencies.extend([123, 321, 456])
+    bank.accounts.extend([account, check_acc])
+    bank.clients.extend([gaius, john])
+
+    bank.auth(account.agency, john, account)
