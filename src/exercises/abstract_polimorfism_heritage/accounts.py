fd6601a9b07882adf4963c9dbdd06eed1be36523
gaaiuss <caio.gui.castro@gmail.com>
2026-08-06 10:28:02 -0300

    add: check account limit logic

--- src/exercises/abstract_polimorfism_heritage/accounts.py
+++ src/exercises/abstract_polimorfism_heritage/accounts.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Account(ABC):
-    def __init__(self, agency: int, account_number: int) -> None:
+    def __init__(self, agency: int, account_number: int, balance: float = 0) -> None:
         self.agency = agency
         self.account_number = account_number
-        self.balance = 0.0
+        self.balance = balance
 
     def deposit(self, value: float) -> None:
         self.balance += value
@@ -15,8 +15,21 @@ class Account(ABC):
 
 
 class CheckingAccount(Account):
+    def __init__(
+        self, agency: int, account_number: int, balance: float = 0, limit: float = 0
+    ) -> None:
+        super().__init__(agency, account_number, balance)
+        self.limit = limit
+
     def draw(self, value: float) -> None:
-        self.balance -= value
+        new_balance = self.balance - value
+        max_limit = -self.limit
+
+        if new_balance < max_limit:
+            print("Insufficient balance/limit to draw.")
+            return
+
+        self.balance = new_balance
 
 
 class SavingsAccount(Account):
@@ -28,3 +41,14 @@ class SavingsAccount(Account):
             return
 
         self.balance = new_balance
+
+
+if __name__ == "__main__":
+    save_acc = CheckingAccount(123, 123, 0, 100)
+    save_acc.deposit(50)
+    save_acc.draw(50)
+    save_acc.draw(50)
+    save_acc.draw(50)
+    save_acc.draw(50)
+
+    print(save_acc.__dict__)
